c330235c7a88b49a10c08e384645523962d8381e
João Pedro Bessegatto <bfjoaop@gmail.com>
2024-09-26 21:31:18 -0300

    Adicionar arquivos de projeto.

---
+++ CarrinhoDeCompras/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace CarrinhoDeCompras
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            lstCompras = new ListBox();
+            lstProdutos = new ListBox();
+            label2 = new Label();
+            label1 = new Label();
+            lbl1 = new Label();
+            lbltotal = new Label();
+            btnAdd = new Button();
+            btnRemove = new Button();
+            btnFim = new Button();
+            SuspendLayout();
+            // 
+            // lstCompras
+            // 
+            lstCompras.Font = new Font("Consolas", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            lstCompras.FormattingEnabled = true;
+            lstCompras.ItemHeight = 19;
+            lstCompras.Location = new Point(407, 144);
+            lstCompras.Name = "lstCompras";
+            lstCompras.Size = new Size(288, 194);
+            lstCompras.TabIndex = 1;
+            // 
+            // lstProdutos
+            // 
+            lstProdutos.Font = new Font("Consolas", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            lstProdutos.FormattingEnabled = true;
+            lstProdutos.ItemHeight = 19;
+            lstProdutos.Location = new Point(79, 144);
+            lstProdutos.Name = "lstProdutos";
+            lstProdutos.Size = new Size(288, 232);
+            lstProdutos.TabIndex = 3;
+            // 
+            // label2
+            // 
+            label2.Location = new Point(407, 126);
+            label2.Name = "label2";
+            label2.Size = new Size(289, 15);
+            label2.TabIndex = 4;
+            label2.Text = "Compras";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(79, 126);
+            label1.Name = "label1";
+            label1.Size = new Size(55, 15);
+            label1.TabIndex = 5;
+            label1.Text = "Produtos";
+            // 
+            // lbl1
+            // 
+            lbl1.BackColor = Color.DodgerBlue;
+            lbl1.Dock = DockStyle.Top;
+            lbl1.Font = new Font("Verdana", 20.25F, FontStyle.Regular, GraphicsUnit.Point);
+            lbl1.Location = new Point(0, 0);
+            lbl1.Name = "lbl1";
+            lbl1.Size = new Size(791, 88);
+            lbl1.TabIndex = 6;
+            lbl1.Text = "Lojinha de eletrônicos";
+            lbl1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // lbltotal
+            // 
+            lbltotal.BackColor = SystemColors.Window;
+            lbltotal.Font = new Font("Consolas", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            lbltotal.Location = new Point(407, 341);
+            lbltotal.Name = "lbltotal";
+            lbltotal.Size = new Size(288, 35);
+            lbltotal.TabIndex = 7;
+            lbltotal.Text = "Total:";
+            lbltotal.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // btnAdd
+            // 
+            btnAdd.Location = new Point(79, 382);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(125, 54);
+            btnAdd.TabIndex = 8;
+            btnAdd.Text = "Adicionar";
+            btnAdd.UseVisualStyleBackColor = true;
+            btnAdd.Click += btnAdd_Click;
+            // 
+            // btnRemove
+            // 
+            btnRemove.Location = new Point(407, 382);
+            btnRemove.Name = "btnRemove";
+            btnRemove.Size = new Size(120, 54);
+            btnRemove.TabIndex = 9;
+            btnRemove.Text = "Remover";
+            btnRemove.UseVisualStyleBackColor = true;
+            btnRemove.Click += btnRemove_Click;
+            // 
+            // btnFim
+            // 
+            btnFim.Location = new Point(584, 382);
+            btnFim.Name = "btnFim";
+            btnFim.Size = new Size(112, 54);
+            btnFim.TabIndex = 10;
+            btnFim.Text = "Finalizar Compra";
+            btnFim.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveBorder;
+            ClientSize = new Size(791, 494);
+            Controls.Add(btnFim);
+            Controls.Add(btnRemove);
+            Controls.Add(btnAdd);
+            Controls.Add(lbltotal);
+            Controls.Add(lbl1);
+            Controls.Add(label1);
+            Controls.Add(label2);
+            Controls.Add(lstProdutos);
+            Controls.Add(lstCompras);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
+            MdiChildrenMinimizedAnchorBottom = false;
+            MinimizeBox = false;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Carrinho de compras";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ListBox lstCompras;
+        private ListBox lstProdutos;
+        private Label label2;
+        private Label label1;
+        private Label lbl1;
+        private Label lbltotal;
+        private Button btnAdd;
+        private Button btnRemove;
+        private Button btnFim;
+    }
+}
---
+++ CarrinhoDeCompras/Form1.cs
@@ -0,0 +1,120 @@
+using System.Collections.Generic;
+using System.Drawing.Text;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using static CarrinhoDeCompras.Form1;
+namespace CarrinhoDeCompras
+{
+    public partial class Form1 : Form
+    {
+        internal class Produtos
+        {
+            public string nome;
+            public double preco;
+        }
+        List<Produtos> ListaCompras = new List<Produtos>();
+        public Form1()
+        {
+            InitializeComponent();
+            SomarCompras();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            List<Produtos> ListaProdutos = new List<Produtos>();
+
+            ListaProdutos = new List<Produtos>
+            {
+                new Produtos() {nome = "celular", preco = 2500.90 },
+                new Produtos() {nome = "Notebook", preco = 2900.80 },
+                new Produtos() {nome = "mouse", preco = 250.50},
+                new Produtos() {nome = "Monitor", preco = 750.00},
+                new Produtos() {nome = "Computador", preco = 4750.99},
+                new Produtos() {nome = "headset", preco = 220.50}
+            };
+            foreach (Produtos p in ListaProdutos)
+            {
+                lstProdutos.Items.Add(Organizador(p));
+            }
+            lstProdutos.Tag = ListaProdutos;
+        }
+        private String Organizador(Produtos p)
+        {
+            return string.Format("{0,-10} {1,20:C2}", p.nome, p.preco);
+        }
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (lstProdutos.SelectedItems.Count > 0)
+            {
+                if (MessageBox.Show(
+                    "Deseja adicionar esse produto ? ",
+                    "Validação",
+                    MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    var produtos = (List<Produtos>)lstProdutos.Tag;
+                    int index = lstProdutos.SelectedIndex;
+                    Produtos produtoSelecionado = produtos[index];
+
+                    lstCompras.Items.Add(Organizador(produtoSelecionado));
+                    ListaCompras.Add(produtoSelecionado);
+                    SomarCompras();
+                }
+
+            }
+            else
+            {
+                MessageBox.Show(
+                    "Selecione um Produto para adicionar!",
+                    "Informação",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning);
+            }
+        }
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            if (lstCompras.SelectedItems.Count > 0)
+            {
+                if (MessageBox.Show(
+                    "Deseja remover esse produto ?",
+                    "Validação",
+                    MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    int index = lstCompras.SelectedIndex;
+
+                    lstCompras.Items.RemoveAt(index);
+                    ListaCompras.RemoveAt(index);
+                    SomarCompras();
+                }
+            }
+            else
+            {
+                MessageBox.Show(
+                    "Selecione um produto para remover!",
+                    "Informação",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning);
+            }
+        }
+        private void SomarCompras()
+        {
+            double total = 0;
+            foreach (var item in lstCompras.Items)
+            {
+                // Acessa a lista original de produtos associada ao lstProdutos.Tag
+                var produtos = (List<Produtos>)lstProdutos.Tag;
+
+                
+                foreach (var produto in produtos)
+                {              
+                    if (Organizador(produto) == item.ToString())
+                    {
+                        total += produto.preco;
+                    }
+                }
+            }
+            lbltotal.Text = "Total:" + total.ToString("R$ " + "0.00");
+        }
+    }
+}
